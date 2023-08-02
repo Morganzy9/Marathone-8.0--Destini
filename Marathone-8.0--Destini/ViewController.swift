0cda9a7cfaf86c559525ddc6b7393c1a76d778e0
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-02 16:34:30 +0500

    Initial Commit

---
+++ Marathone-8.0--Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathone-8.0--Destini
+//
+//  Created by Ислам Пулатов on 8/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
